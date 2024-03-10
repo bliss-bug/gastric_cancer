3d29386d42c668f317c08c6d490165164a7a869f
bliss-bug <191220184@smail.nju.edu.cn>
2024-03-11 00:01:33 +0800

    update

--- yolo_train.py
+++ yolo_train.py
@@ -6,22 +6,21 @@ from ultralytics import YOLO
 def main(args):
     model = YOLO("yolov8n.pt")
 
-    model.train(data="data.yaml", epochs=args.epochs, imgsz=args.image_size, 
+    results = model.train(data="data.yaml", epochs=args.epochs, imgsz=args.image_size, 
                 device=args.device_ids, batch=args.batch_size, optimizer='AdamW',
                 cache=True)  # train the model
-    metrics = model.val()  # evaluate model performance on the validation set
+    #metrics = model.val()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--num_classes', type=int, default=2)
-    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--epochs', type=int, default=40)
     parser.add_argument('--batch_size', type=int, default=64)
 
-    parser.add_argument('--device', type=str, default='cuda:0')
     parser.add_argument('--device_ids', nargs='+', type=int, default=[0,1])
-    parser.add_argument('--image_size', type=int, default=512, help="target input image size")
+    parser.add_argument('--image_size', type=int, default=512)
 
     args = parser.parse_args()
 
